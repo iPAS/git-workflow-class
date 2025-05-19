bb17a72f3653a8da1d8f5ef63822d11959b9180a
iPAS <ptiwatthanont@gmail.com>
2025-05-19 13:06:55 +0700

    fix: enable commands by add_typer(..)

--- team-task/team0/cli.py
+++ team-task/team0/cli.py
@@ -4,8 +4,8 @@ from commands import fetch, plot, news
 
 app = typer.Typer()
 # TODO: Implement then uncomment me
-# app.add_typer(fetch.app, name="fetch")
-# app.add_typer(plot.app, name="plot")
+app.add_typer(fetch.app, name="fetch")
+app.add_typer(plot.app, name="plot")
 # app.add_typer(news.app, name="news")
 
 
